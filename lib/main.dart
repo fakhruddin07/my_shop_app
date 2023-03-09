2bff6a9147f24e236a0da7ae10017936317bbc2b
fakhruddin07 <fakhruddinnoman95@gmail.com>
2023-03-09 21:17:06 +0600

    Working on the Products Grid & Item Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_shop/screens/products_overview_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,23 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("My Shop"),
-      ),
-      body: Center(
-        child: Text("Let\'s build a shop."),
-      ),
+      home: ProductOverviewScreen(),
     );
   }
 }
