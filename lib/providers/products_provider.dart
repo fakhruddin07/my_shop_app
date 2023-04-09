87b9972c2e4a1510ab5924e2937e8a4b4a5c8730
fakhruddin07 <fakhruddinnoman95@gmail.com>
2023-04-10 03:25:26 +0600

    Allowing Users to Delete Products

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -84,4 +84,9 @@ class ProductsProvider with ChangeNotifier {
       print("object");
     }
   }
+
+  void deleteProduct(String id) {
+    _items.removeWhere((prod) => prod.id == id);
+    notifyListeners();
+  }
 }
--- lib/widgets/user_products_item.dart
+++ lib/widgets/user_products_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:my_shop/providers/products_provider.dart';
 import 'package:my_shop/screens/edit_product_screen.dart';
+import 'package:provider/provider.dart';
 
 class UserProductItem extends StatelessWidget {
   final String id;
@@ -26,7 +28,10 @@ class UserProductItem extends StatelessWidget {
             color: Theme.of(context).colorScheme.primary,
           ),
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              Provider.of<ProductsProvider>(context, listen: false)
+                  .deleteProduct(id);
+            },
             icon: Icon(
               Icons.delete,
               color: Theme.of(context).colorScheme.error,
